86b799e0722ff76287d183f84c9de5060e9e6fd9
turborvip <turborvipjits>
2022-09-23 17:08:18 +0700

    push

--- src/helper/localStorage.tsx
+++ src/helper/localStorage.tsx
@@ -1,15 +1,15 @@
-const add = (key:string,values:string):void => {
-    localStorage.setItem(key,values);
-}
-const get = (key:string) => {
-    // JSON.parse(localStorage.getItem(key)as string);  ! : Non-null assertion operator
-    return JSON.parse(localStorage.getItem(key)!);
-}
+const add = (key: string, values: string): void => {
+  localStorage.setItem(key, values);
+};
+const get = (key: string) => {
+  // JSON.parse(localStorage.getItem(key)as string);  ! : Non-null assertion operator
+  return JSON.parse(localStorage.getItem(key)!);
+};
 const clear = () => {
-    return localStorage.clear()
-}
+  return localStorage.clear();
+};
 export default {
-    add,
-    get,
-    clear
-}
+  add,
+  get,
+  clear,
+};
